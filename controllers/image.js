80c08983792dcb73c32a38c3ab8ef2031c74cd2b
VishalMandalia <vishal.smandalia001@gmail.com>
2020-04-24 16:17:52 +0530

    deployment fix

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+ apiKey: '4849e807ac2e4aec9d0405d4383af326'
 });
 
 const handleApiCall = (req, res) => {
