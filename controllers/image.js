f8b6af1eb42b06c8754f4d601a692f8c7041c81a
VishalMandalia <vishal.smandalia001@gmail.com>
2020-05-03 18:36:15 +0530

    dd

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({
 
 const handleApiCall = (req, res) => {
   app.models
-    .predict(Clarifai.Face Detection, req.body.input)
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
     })
